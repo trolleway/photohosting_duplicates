d9cf869f8063e7a009b3f000b98fcddb759af49b
Artem Svetlov <trolleway@yandex.ru>
2017-12-12 23:20:13 +0300

    remove russian comments

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 ﻿
-
 import os
-
-
 import sys
 import datetime
 import pyexiv2
@@ -32,16 +29,12 @@ class Publicator:
         return None
         
     def get_photo_timestamp(self,exif_data):
-        """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
         caption = ""
         caption=self._get_if_exist(exif_data, "EXIF:DateTimeOriginal")
         
         return caption
-    
-   
-    
+
     def cycle_folder(self,folder):
-    #Прочитать список фото в каталоге
         photos=[]
         cnt=1
         for dirpath, dnames, fnames in os.walk(folder):
@@ -53,48 +46,34 @@ class Publicator:
         self.search_at_flickr(photos)
                     
     def cycle_files(self,photos):
-    #Прочитать список фото в каталоге
-        
         self.search_at_flickr(photos)
     
     def search_at_flickr(self,photos):
-        
-        #Прочитать дату сьёмки, наличие подписи
         for photo in photos:
-
             exiftool.executable ='exiftool.exe'
             with exiftool.ExifTool() as et:
-                #metadata = et.get_metadata(photo['filepath'])
                 metadata = et.get_metadata(photo)
                 datetimestring = self.get_photo_timestamp(metadata)
-
                 
                 l = list(datetimestring)
                 l[4] = '-'
                 l[7] = '-'
                 datetimestring=''.join(l)
 
-                #print datetimestring[4]
-                #datetimestring[8]='-'
-
                 dt = dateparser.parse(datetimestring)
 
                 timestamp = int(time.mktime(dt.timetuple()))
                 timestamp = dt.strftime('%Y-%m-%d %H:%M:%S')
-
-                #Поискать это фото по времени на flickr
-                
+        
                 flickr_instance = flickr()
                 flickr_state = flickr_instance.search_flickr(timestamp)
                 print str(photo).ljust(50),str(flickr_state).ljust(3)
-        #вывести на экран
-
+        
         
 if __name__ == "__main__":        
     files=[]
     cnt=0
     for arg in sys.argv:
-        #print arg
         if cnt>0:
             #arge=arg.decode(sys.getfilesystemencoding())
             files.append(arg)
